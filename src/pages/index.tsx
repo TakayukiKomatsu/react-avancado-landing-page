216e395203eb351cf9e3878cfea3bcc8d21ad54b
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-10-15 11:54:35 -0300

    feat: :sparkles: update SectionAboutProject to use dynamic Values from GraphQL request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,10 @@ import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/query/GET_LANDING_PAGE'
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo, header }: LandingPageProps) => (
+const Index = ({ logo, header, sectionAboutProject }: LandingPageProps) => (
   <>
     <SectionHero logo={logo} header={header} />
-    <SectionAboutProject />
+    <SectionAboutProject {...sectionAboutProject} />
     <SectionTech />
     <SectionConcepts />
     <SectionModules />
