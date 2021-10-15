846c6ad5973de4a7b5d8099829f83edbd4424c2e
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-10-15 11:20:33 -0300

    feat: :label: add HeaderProps types

--- src/types/api.ts
+++ src/types/api.ts
@@ -3,6 +3,20 @@ export type LogoProps = {
   url: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    url: string
+    label: string
+  }
+  media: {
+    alternativeText: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
