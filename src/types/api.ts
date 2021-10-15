c4b3cf5825caacdade37851d86823af29fb3e738
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-10-15 12:06:22 -0300

    feat: :label: add sectionTechProps types

--- src/types/api.ts
+++ src/types/api.ts
@@ -25,8 +25,21 @@ export type SectionAboutProjectProps = {
   }
 }
 
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
 }
